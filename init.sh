fc396227961901fe80b3de0908dd611c9b8cd963
Kai Yu <finno@live.cn>
2015-06-11 23:47:48 +0800

    refine the init script

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+rm -rf ~/.vimrc ~/.vim
+mv `pwd` ~/.vim
+cd ~/.vim
 ln -s `pwd`/vimrc ~/.vimrc
 git submodule update --init --recursive
 
-echo "# colorscheme" >> ~/.bashrc
-echo "export TERM=xterm-256color" >> ~/.bashrc
